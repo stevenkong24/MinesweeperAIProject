3198b72bd4526fe9dc6103d4083fc5935cd2f9f8
stevenkong24 <steven.kong2411@gmail.com>
2024-11-29 00:44:49 -0500

    game working in terminal

--- game/board.py
+++ game/board.py
@@ -2,6 +2,7 @@ import random
 
 import numpy as np
 import game
+import pygame
 
 def surrounding_points(x, y):
     return [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1), (x + 1, y + 1), (x - 1, y - 1), (x + 1, y - 1),
@@ -79,42 +80,73 @@ def get_dimensions(grid):
     return(len(grid), len(grid[0])) 
 
 if __name__ == '__main__':
-    grid = generate(30, 16, 20)
+    # pygame.init()
+    # screen = pygame.display.set_mode((800, 600))
+    
+    uncovered_grid = generate(30, 16, 1)
     cur_grid = generate_covered(30, 16)
     
-    label_grid = create_label_grid(grid)
+    label_grid = create_label_grid(uncovered_grid)
     # grid = random_coverage(grid)
-    for r in grid:
-        frmt = "{:>3}" * len(r)
-        print(frmt.format(*r))
-    
-    
-    for r in cur_grid:
+    for r in uncovered_grid:
         frmt = "{:>3}" * len(r)
         print(frmt.format(*r))
     
     print()
-    x = 10
-    y = 5
-    cur_grid = game.reveal_square(x, y, cur_grid, grid)
+    print("   0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5")
+    print()
+    count = 0
     for r in cur_grid:
         frmt = "{:>3}" * len(r)
-        print(frmt.format(*r))
+        if count < 10:
+            print(" " + str(count) + frmt.format(*r))
+        else:
+            print(str(count) + frmt.format(*r))
+        count += 1
+    
+    print()
+    x = 10
+    y = 5
+    # cur_grid = game.reveal_square(x, y, cur_grid, grid)
     #cur_grid = game.flagSquare(0, 5, cur_grid)
-    '''
+    
     while isinstance(cur_grid, list):
-        decision = input("R to reveal square, F to flag square: ")
+        decision = input("R to reveal square, F to flag square, C to clear squares around square: ")
         coords = input("X, Y coordinates of desired square: ")
         coords = tuple(map(int, coords.split(', ')))
         x, y = coords
         if (decision == 'R'):
-            cur_grid = game.reveal_square(x, y, cur_grid, grid)
+            cur_grid = game.reveal_square(x, y, cur_grid, uncovered_grid)
         elif (decision == 'F'):
-            cur_grid = game.flag_square(x, y, cur_grid, grid)
+            cur_grid = game.flag_square(x, y, cur_grid)
+        elif (decision == 'C'):
+            cur_grid = game.clear_square(x, y, cur_grid, uncovered_grid)
+        
+        if cur_grid == False:
+            break
+        print()
+        print("    0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5")
+        print()
+        
+        count = 0
         for r in cur_grid:
             frmt = "{:>3}" * len(r)
-            print(frmt.format(*r))
+            if count < 10:
+                print(" " + str(count) + frmt.format(*r))
+            else:
+                print(str(count) + frmt.format(*r))
+            count += 1
 
-        print(str(game.mines_remaining(cur_grid)) + " mines remaining")
-    '''
+        print(str(game.mines_remaining(cur_grid, uncovered_grid)) + " mines remaining")
+        print()
+        if game.mines_remaining(cur_grid, uncovered_grid) == 0:
+            cur_grid = True
+            break
+        # screen.fill((0, 0, 255))
+        # pygame.display.flip()
+    if cur_grid:
+        print("You won")
+    else:
+        print("You lost")
+    
     
--- game/game.py
+++ game/game.py
@@ -30,9 +30,42 @@ def reveal_square(x, y, cur_grid, uncovered_grid):
         # print(board.get_surroundings(x, y, uncovered_grid))
 
 def flag_square(x, y, cur_grid):
-    if isinstance(cur_grid[x][y], str):
+    if isinstance(cur_grid[x][y], str) and cur_grid[x][y] == "#":
         cur_grid[x][y] = "!"
+    elif isinstance(cur_grid[x][y], str) and cur_grid[x][y] == "!":
+        cur_grid[x][y] = "#"
     return cur_grid
 
-def mines_remaining(cur_grid):
-    return sum(element == 0 for row in cur_grid for element in row)
+def mines_remaining(cur_grid, uncovered_grid):
+    return sum(element == -1 for row in uncovered_grid for element in row) - sum(element == '!' for row in cur_grid for element in row)
+
+def clear_square(x, y, cur_grid, uncovered_grid):
+    # Error here
+    if (board.get_surroundings(x, y, cur_grid).count("!") == cur_grid[x][y]):
+        # use surrounding points instead
+        size = board.get_dimensions(cur_grid)
+        
+        print(board.get_surroundings(x, y, cur_grid))
+        for coord in board.surrounding_points(x, y):
+            print(coord)
+            if x < size[0] and y < size[1]:
+                coord_x, coord_y = coord
+                if (cur_grid[coord_x][coord_y] != "!"):
+                    cur_grid = reveal_square(coord_x, coord_y, cur_grid, uncovered_grid)
+                    
+                    if not cur_grid:
+                        return False
+
+        '''
+        if (0 in board.get_surroundings(x, y, uncovered_grid)):
+                for coord in board.surrounding_points(x, y):
+                    x, y = coord
+                    # print(x)
+                    # print(y)
+                    # if x < size[0] and y < size[1] and uncovered_grid[x][y] == 0 and cur_grid[x][y] == "#":
+                    if x < size[0] and y < size[1] and uncovered_grid[x][y] != -1 and cur_grid[x][y] == "#" and 0 in board.get_surroundings(x, y, uncovered_grid):
+                        cur_grid[x][y] = uncovered_grid[x][y]
+                        toSearch.append((x, y))
+                        
+        '''
+    return cur_grid
