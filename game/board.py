e35d59b2e1cee3d22b41d926e597f1936dd0a85e
Moez B <35158270+treephones@users.noreply.github.com>
2024-12-05 17:01:36 -0500

    heatmap working

--- game/board.py
+++ game/board.py
@@ -3,6 +3,8 @@ import random
 import numpy as np
 import pygame
 
+import tensorflow
+
 
 def reveal_square(x, y, cur_grid, uncovered_grid):
     size = get_dimensions(cur_grid)
@@ -86,8 +88,11 @@ def check_if_correct(cur_grid, uncovered_grid):
                 return False
     return True
 
+
 MINE_CONST = 100
 COVERED_CONST = -1
+
+
 def generateForData(n, m, n_mines):
     grid = [[0] * m for _ in range(n)]
     mines = set()
@@ -96,13 +101,14 @@ def generateForData(n, m, n_mines):
         x = random.randint(0, m - 1)
         if (y, x) not in mines:
             mines.add((y, x))
-            grid[y][x] = MINE_CONST #-1
+            grid[y][x] = MINE_CONST  # -1
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] != MINE_CONST:
                 surroundings = get_surroundings(i, j, grid)
                 grid[i][j] = surroundings.count(MINE_CONST)
     return grid
+
 
 def surrounding_points(x, y):
     return [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1), (x + 1, y + 1), (x - 1, y - 1), (x + 1, y - 1),
@@ -139,21 +145,23 @@ def generate(n, m, n_mines):
             rect = pygame.Rect(10 + i * cell_size, 10 + j * cell_size, cell_size + 1, cell_size + 1)
             # pygame.draw.rect(screen, (211, 211, 211), rect)
             pygame.draw.rect(screen, (50, 50, 50), rect, 1)
-            
+
             rect = pygame.Rect(770 + i * cell_size, 10 + j * cell_size, cell_size + 1, cell_size + 1)
             # pygame.draw.rect(screen, (211, 211, 211), rect)
             pygame.draw.rect(screen, (50, 50, 50), rect, 1)
             # pygame.draw.rect(screen, GRAY, rect, 1)  # Draw grid cell with a thin border
-            
+
             if grid[i][j] != -1:
                 surroundings = get_surroundings(i, j, grid)
                 grid[i][j] = surroundings.count(-1)
     pygame.display.flip()
     return grid
 
+
 def generate_covered(n, m):
     grid = [["#"] * m for _ in range(n)]
     return grid
+
 
 def random_coverage(grid):
     covered_grid = generate_covered(len(grid), len(grid[0]))
@@ -180,6 +188,7 @@ def create_label_grid(grid):
                 label_grid[i][j] = 1
     return label_grid
 
+
 def create_coverage_mask(covered_grid):
     new_covered_grid = covered_grid.copy()
     for i in range(len(covered_grid)):
@@ -190,10 +199,11 @@ def create_coverage_mask(covered_grid):
                 new_covered_grid[i][j] = 0
     return new_covered_grid
 
+
 def create_data(n, m, n_mines, amount):
     boards = []
     for i in range(amount):
-        print(f'generated data point {i+1}/{amount}')
+        print(f'generated data point {i + 1}/{amount}')
         grid = generateForData(n, m, n_mines)
         # for r in grid:
         #     frmt = "{:>3}" * len(r)
@@ -213,18 +223,19 @@ def create_data(n, m, n_mines, amount):
         grid = np.array(grid)
         grid = np.expand_dims(grid, axis=-1)
         grid = np.expand_dims(grid, axis=-0)
-        #print(grid)
+        # print(grid)
         label_grid = np.array(label_grid)
         label_grid = np.expand_dims(label_grid, axis=-1)
         label_grid = np.expand_dims(label_grid, axis=-0)
-        #print(label_grid)
-        coverage_map= np.array(coverage_map)
+        # print(label_grid)
+        coverage_map = np.array(coverage_map)
         coverage_map = np.expand_dims(coverage_map, axis=-1)
         coverage_map = np.expand_dims(coverage_map, axis=-0)
-        #print(coverage_map)
+        # print(coverage_map)
 
         boards.append([grid, label_grid, coverage_map])
     return boards
+
 
 def set_dimensions(matrix):
     matrix = np.array(matrix)
@@ -232,34 +243,52 @@ def set_dimensions(matrix):
     matrix = np.expand_dims(matrix, axis=-0)
     return matrix
 
-def get_dimensions(grid):
-    return(len(grid), len(grid[0])) 
 
-def update_board(cur_grid):
-    print(cur_grid)
+def get_dimensions(grid):
+    return (len(grid), len(grid[0]))
+
+def disp_grid_to_model_grid(cur_grid):
+    grid = [[0]*len(cur_grid[0]) for _ in range(len(cur_grid))]
     for i in range(len(cur_grid)):
         for j in range(len(cur_grid[0])):
-            
+            if (cur_grid[i][j] == '#'):
+                grid[i][j] = -1
+            elif (cur_grid[i][j] == '!'):
+                grid = 100
+            else:
+                grid[i][j] = int(cur_grid[i][j])
+    return grid
+
+def update_board(cur_grid, model):
+    print(cur_grid)
+    mgrid = disp_grid_to_model_grid(cur_grid)
+    mgrid = set_dimensions(mgrid)
+    ans = model.predict(mgrid)
+    ans = [[round(float(val), 2) for val in row] for row in ans.squeeze()]
+    for i in range(len(cur_grid)):
+        for j in range(len(cur_grid[0])):
+
             rect = pygame.Rect(10 + i * cell_size, 10 + j * cell_size, cell_size + 1, cell_size + 1)
             rect2 = pygame.Rect(770 + i * cell_size, 10 + j * cell_size, cell_size + 1, cell_size + 1)
             # pygame.draw.rect(screen, (211, 211, 211), rect)
             if (cur_grid[i][j] == '#'):
                 pygame.draw.rect(screen, (220, 220, 220), rect)
-                pygame.draw.rect(screen, (220, 220, 220), rect2)
+                pygame.draw.rect(screen, (220*ans[i][j], 0, 0), rect2)
             elif (cur_grid[i][j] == 0):
                 pygame.draw.rect(screen, (255, 255, 255), rect)
-                pygame.draw.rect(screen, (255, 255, 255), rect2)
+                pygame.draw.rect(screen, (220*ans[i][j], 0, 0), rect2)
             elif (cur_grid[i][j] == '!'):
                 pygame.draw.rect(screen, (220, 220, 220), rect)
-                pygame.draw.rect(screen, (220, 220, 220), rect2)
+                pygame.draw.rect(screen, (220*ans[i][j], 0, 0), rect2)
             else:
                 # Fix this
-                # screen.blit(my_font.render(str(game.mines_remaining(cur_grid, uncovered_grid)), False, (0, 0, 0)), (i * cell_size + 15, j * cell_size)) 
+                # screen.blit(my_font.render(str(game.mines_remaining(cur_grid, uncovered_grid)), False, (0, 0, 0)), (i * cell_size + 15, j * cell_size))
                 print(get_surroundings(i, j, uncovered_grid))
                 pygame.draw.rect(screen, (255, 255, 255), rect)
                 pygame.draw.rect(screen, (255, 255, 255), rect2)
-                screen.blit(my_font.render(str(get_surroundings(i, j, uncovered_grid).count(-1)), False, (0, 0, 0)), (i * cell_size + 15, j * cell_size))    
-                
+                screen.blit(my_font.render(str(get_surroundings(i, j, uncovered_grid).count(-1)), False, (0, 0, 0)),
+                            (i * cell_size + 15, j * cell_size))
+
             pygame.draw.rect(screen, (0, 0, 0), rect, 1)
             pygame.draw.rect(screen, (0, 0, 0), rect2, 1)
             # pygame.draw.rect(screen, GRAY, rect, 1)  # Draw grid cell with a thin border
@@ -276,22 +305,24 @@ if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode((width, height))
     screen.fill((255, 255, 255))
-    pygame.font.init() 
+    pygame.font.init()
     my_font = pygame.font.SysFont('Comic Sans MS', 30)
-    
+
+    model = tensorflow.keras.models.load_model("../bestsofar.keras")
+
     uncovered_grid = generate(columns, rows, mines)
     cur_grid = generate_covered(columns, rows)
     label_grid = create_label_grid(uncovered_grid)
     # grid = random_coverage(grid)
-    
 
-    screen.blit(my_font.render(str(mines_remaining(cur_grid, uncovered_grid)) + " left", False, (0, 0, 0)), ((columns - 4) * cell_size, (rows + 1) * cell_size))
+    screen.blit(my_font.render(str(mines_remaining(cur_grid, uncovered_grid)) + " left", False, (0, 0, 0)),
+                ((columns - 4) * cell_size, (rows + 1) * cell_size))
     pygame.display.flip()
-    
+
     for r in uncovered_grid:
         frmt = "{:>3}" * len(r)
         print(frmt.format(*r))
-    
+
     print()
     print("   0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5")
     print()
@@ -303,13 +334,13 @@ if __name__ == '__main__':
         else:
             print(str(count) + frmt.format(*r))
         count += 1
-    
+
     print()
     x = 10
     y = 5
     # cur_grid = game.reveal_square(x, y, cur_grid, grid)
-    #cur_grid = game.flagSquare(0, 5, cur_grid)
-    
+    # cur_grid = game.flagSquare(0, 5, cur_grid)
+
     while isinstance(cur_grid, list):
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -317,12 +348,12 @@ if __name__ == '__main__':
 
                 x, y = pygame.mouse.get_pos()
                 # rint(width//)
-                
+
                 row = int((y - 10) // cell_size)
                 col = int((x - 10) // cell_size)
                 if row > rows or row < 0 or col > columns or columns < 0:
                     break
-                
+
                 if event.button == 1:
                     if cur_grid[col][row] == '#':
                         cur_grid = reveal_square(col, row, cur_grid, uncovered_grid)
@@ -331,17 +362,16 @@ if __name__ == '__main__':
 
                 elif event.button == 3:
                     cur_grid = flag_square(col, row, cur_grid)
-               
-                
+
                 if cur_grid == False:
                     break
 
-                update_board(cur_grid)
+                update_board(cur_grid, model)
 
                 print()
                 print("    0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5")
                 print()
-                
+
                 count = 0
                 for r in cur_grid:
                     frmt = "{:>3}" * len(r)
@@ -352,12 +382,13 @@ if __name__ == '__main__':
                     count += 1
 
                 print(str(mines_remaining(cur_grid, uncovered_grid)) + " mines remaining")
-                
+
                 rect = pygame.Rect(770 + columns * cell_size, 0, cell_size + 1, cell_size + 1)
                 # pygame.draw.rect(screen, (211, 211, 211), rect)
                 pygame.draw.rect(screen, (255, 255, 255), rect)
-                #pygame.display.update(pygame.Rect((columns + 1)* cell_size, 0, 100, 100))
-                screen.blit(my_font.render(str(mines_remaining(cur_grid, uncovered_grid)) + " left", False, (0, 0, 0)), ((columns - 4) * cell_size, (rows + 1) * cell_size))
+                # pygame.display.update(pygame.Rect((columns + 1)* cell_size, 0, 100, 100))
+                screen.blit(my_font.render(str(mines_remaining(cur_grid, uncovered_grid)) + " left", False, (0, 0, 0)),
+                            ((columns - 4) * cell_size, (rows + 1) * cell_size))
                 # screen.blit(my_font.render(str(game.mines_remaining(cur_grid, uncovered_grid)) + " left", False, (0, 0, 0)), ((columns + 1) * cell_size, 0))
                 print()
                 if mines_remaining(cur_grid, uncovered_grid) == 0 and check_if_correct(cur_grid, uncovered_grid):
@@ -369,5 +400,4 @@ if __name__ == '__main__':
         print("You won")
     else:
         print("You lost")
-    
-    
+
